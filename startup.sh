9e8d7f58c28cc32d89cef0c1b140dfbdebf1306e
tcruz <tcruz@corp.globo.com>
2020-08-02 20:19:06 -0300

    feat update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -p 8888:8888 datacamp /bin/bash -c "/opt/conda/bin/conda install jupyter pandas -y --quiet && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks/ --ip='0.0.0.0' --port=8888 --no-browser --allow-root" -v ./notebooks/:/opt/notebooks/
+docker run -it -p 8888:8888 datacamp /bin/bash -c "/opt/conda/bin/conda install jupyter pandas -y --quiet && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks/ --ip='0.0.0.0' --port=8888 --no-browser --allow-root" -v "$PWD/notebooks":/opt/notebooks/ --name=datacamp
